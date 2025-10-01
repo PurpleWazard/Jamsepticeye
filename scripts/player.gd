c4f35da5a5de2729f2a63311f6a342688b5e7577
Wazard <purplewazard@proton.me>
2025-10-01 14:46:07 -0500

    player can now run

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,19 @@
-extends CharacterBody2D
+extends StaticBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+var SPEED = 300.0
+var RUN_SPEED = 800.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	# if not is_on_floor():
-	#	velocity += get_gravity() * delta
+	var local_speed = SPEED
+	var direction: Vector2
+	if Input.is_action_pressed("ui_run"):
+		local_speed = RUN_SPEED
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.y = Input.get_axis("ui_up", "ui_down")
+	if direction.length() > 0:
+		direction = direction.normalized()
+	
+	position += direction * local_speed * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
