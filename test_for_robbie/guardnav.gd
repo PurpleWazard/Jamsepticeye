fbd8624ba2533bfa6e9b34805671d7c0cebe23a3
Ryan dendrick <warrenrobbie22@gmail.com>
2025-10-04 01:00:41 +1000

    g

---
+++ test_for_robbie/guardnav.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+@onready var Nav_agent: NavigationAgent2D = $NavigationAgent2D
+
+func _physics_process(delta: float) -> void:
+	pass
+
+
+func manual_nav() -> void:
+	pass
---
+++ test_for_robbie/spots.gd
@@ -0,0 +1,7 @@
+extends Node2D
+@export var nodes: Array[NodePath]
+@onready var Nav_agent: NavigationAgent2D = $guard/NavigationAgent2D
+
+func _process(delta: float) -> void:
+	Nav_agent.
+	pass
